16f2276ae0fa699196f52945251cca1707841548
Kevin S. Miller <ksmiller99@users.noreply.github.com>
2016-10-19 00:20:44 -0400

    Add files via upload

---
+++ MySql/CreateUser.sql
@@ -0,0 +1,5 @@
+CREATE USER 'WA_DbUser'@'localhost' IDENTIFIED BY  'secretpassword1';
+
+GRANT USAGE ON * . * TO  'WA_DbUser'@'localhost' IDENTIFIED BY  'secretpassword1' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+
+GRANT ALL PRIVILEGES ON  `csit537_project1` . * TO  'WA_DbUser'@'localhost';
