2ba814f19f4dacf9274732a51b960af19b1142ca
Kevin S. Miller <ksmiller99@gmail.com>
2016-10-27 14:40:30 -0400

    Completed registration in User model; User table modifications

--- MySql/csit537_project1.sql
+++ MySql/csit537_project1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2016 at 05:50 AM
+-- Generation Time: Oct 27, 2016 at 08:34 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.19
 
@@ -83,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `wa_product_categories` (
   `CATEGORY_NAME` varchar(20) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `CATEGORY_NAME` (`CATEGORY_NAME`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `wa_product_categories`
@@ -91,6 +91,8 @@ CREATE TABLE IF NOT EXISTS `wa_product_categories` (
 
 INSERT INTO `wa_product_categories` (`ID`, `CATEGORY_NAME`) VALUES
 (9, 'Category 5'),
+(12, 'Category 7'),
+(13, 'category 8'),
 (1, 'Test Category'),
 (2, 'Test Category 3'),
 (3, 'Test Category 4');
@@ -136,7 +138,7 @@ CREATE TABLE IF NOT EXISTS `wa_users` (
   `COMPANY` varchar(25) DEFAULT NULL,
   `ADDRESS_1` varchar(25) NOT NULL,
   `ADDRESS_2` varchar(25) DEFAULT NULL,
-  `CTY` varchar(25) NOT NULL,
+  `CITY` varchar(25) NOT NULL,
   `STATE` varchar(2) NOT NULL,
   `ZIP_CODE` varchar(10) NOT NULL,
   `PHONE_NUMBER` varchar(10) NOT NULL,
@@ -146,8 +148,20 @@ CREATE TABLE IF NOT EXISTS `wa_users` (
   `PRESENTER` tinyint(1) NOT NULL DEFAULT '0',
   `STUDENT` tinyint(1) NOT NULL DEFAULT '0',
   `REVIEWER` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Users of the WA Web App' AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `EMAIL` (`EMAIL`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Users of the WA Web App' AUTO_INCREMENT=28 ;
+
+--
+-- Dumping data for table `wa_users`
+--
+
+INSERT INTO `wa_users` (`ID`, `PW_HASH`, `FIRST_NAME`, `LAST_NAME`, `COMPANY`, `ADDRESS_1`, `ADDRESS_2`, `CITY`, `STATE`, `ZIP_CODE`, `PHONE_NUMBER`, `EMAIL`, `ADMIN`, `ATTENDEE`, `PRESENTER`, `STUDENT`, `REVIEWER`) VALUES
+(1, '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Kevin', 'Miller', 'company', '123 Main', 'Line2', 'MyCity', 'MY', '12345', '1234567890', 'a@a.com', 0, 1, 0, 0, 0),
+(20, '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Kevin', 'Miller', 'Comp', 'Add1', 'Add2', 'Mycity', 'MY', '12345', '1234567890', 'neither@a.com', 0, 1, 0, 0, 0),
+(23, '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Kevin', 'Miller', 'Comp', 'Add1', 'Add2', 'Mycity', 'MY', '12345', '1234567890', 'student@a.com', 0, 1, 0, 1, 0),
+(26, '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Kevin', 'Miller', 'Comp', 'Add1', 'Add2', 'Mycity', 'MY', '12345', '1234567890', 'presenter@a.com', 0, 1, 1, 0, 0),
+(27, '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Kevin', 'Miller', 'comp2', 'aDD1', 'Add2', 'A City', 'NY', '54321', '9876543210', 'b@b.com', 0, 1, 0, 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
