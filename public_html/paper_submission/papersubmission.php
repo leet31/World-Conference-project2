4c6f32a26630eb6dbbe14d0b7d70e3618dd6021f
Tiffany <Tiffany@10.17.158.86>
2016-11-10 19:23:53 -0500

    Update Online registration, font size, and style layout.

--- public_html/paper_submission/papersubmission.php
+++ public_html/paper_submission/papersubmission.php
@@ -3,11 +3,12 @@
     <head>
         <meta charset="utf-8">
         <title>Page Submission</title>
-        <link rel="stylesheet" type="text/css" href="../css/styles.css">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
 
     </head>
 
-    <body background="../images/2015_AIGA-Design-Month_Website-Footer.png" style="text-align: center; font-size: large;">
+    <body background="../images/2015_AIGA-Design-Month_Website-Footer.png"> 
+          <p style="text-align: center; font-size: large;"</p>
         <?php echo(file_get_contents('.\menu.html')) ?>
         UNDER Construction........
     </body>
