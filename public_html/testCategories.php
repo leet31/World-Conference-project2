865e6904bbfd5b814883b4097720783a1587b4ba
Tiffany <Tiffany@10.17.158.86>
2016-11-10 19:23:26 -0500

    Update Online registration, font size, and style layout.

--- public_html/testCategories.php
+++ public_html/testCategories.php
@@ -16,7 +16,7 @@ $cat_ra = $PCM->getList();
         <title>Test Category Model</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" type="text/css" href="./styles.css">
+            <link rel="stylesheet" type="text/css" href="css/styles.css">
     
     </head>
     <body>
