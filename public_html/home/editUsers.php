0b22c43d1f492c4f2a63a0163302248fe45487fb
Kevin S. Miller <ksmiller99@gmail.com>
2016-11-11 15:15:42 -0500

    Finished editUser

--- public_html/home/editUsers.php
+++ public_html/home/editUsers.php
@@ -1,33 +1,12 @@
 <!DOCTYPE html>
-<!-- temp PHP for debugging html-->
 <?php
-//$userID       =isset($_POST['userID'])      ? $_POST['userID']      :"1";
-//$firstName    =isset($_POST['firstName'])   ? $_POST['firstName']   :"John";
-//$lastName     =isset($_POST['lastName'])    ? $_POST['lastName']    :"Doe";
-//$company      =isset($_POST['company'])     ? $_POST['company']     :"Bigly Corp";
-//$address1     =isset($_POST['address1'])    ? $_POST['address1']    :"123MainSt.";
-//$address2     =isset($_POST['address2'])    ? $_POST['address2']    :"Apt.23G";
-//$city         =isset($_POST['city'])        ? $_POST['city']        :"Myfair City";
-//$state        =isset($_POST['state'])       ? $_POST['state']       :"KY";
-//$zipCode      =isset($_POST['zipCode'])     ? $_POST['zipCode']     :"54321";
-//$phoneNumber  =isset($_POST['phoneNumber']) ? $_POST['phoneNumber'] :"(123)456-7890";
-//$email        =isset($_POST['email'])       ? $_POST['email']       :"john.doe@bigly.com";
-//$admin        =isset($_POST['admin']    )   ? $_POST['admin']       :"0";
-//$attendee     =isset($_POST['attendee'] )   ? $_POST['attendee']    :"0";
-//$presenter    =isset($_POST['presenter'])   ? $_POST['presenter']   :"0";
-//$student      =isset($_POST['student']  )   ? $_POST['student']     :"0";
-//$reviewer     =isset($_POST['reviewer'] )   ? $_POST['reviewer']    :"0";
-?>
-
-<?php
-require '../controllers/connectDb.php';
-require '../models/UserModel.php';
+require '../../controllers/connectDb.php';
+require '../../models/UserModel.php';
 
 $UM = new UserModel($pdo);
 
 //perform requested action, if any
 $errMsg = $UM->doAction();
-//echo($UM);
 
 //get user list as an array
 $allList = $UM->getList();
@@ -35,7 +14,41 @@ $allList = $UM->getList();
 <html>
     <head>
         <title>Edit Users</title>
-        <meta charset="UTF-8">
+        <script>
+            function setPwButtonStyle(){
+                var pwHashInput = document.getElementById('pwHash').value;
+                var hiddenPw = document.getElementById('hiddenPw').value;
+                
+                if(hiddenPw.length < 6 && pwHashInput.length < 40 ){
+                    document.getElementById('btnPassword').value="Set PW";
+                    document.getElementById('btnPassword').style.color='red';
+                }else{
+                    document.getElementById('btnPassword').value="Change PW";
+                    document.getElementById('btnPassword').style.color='black';
+                }
+            }
+            
+            function setPw(){
+                var pw1 = prompt("Input new password:");
+                var pw2 = prompt("Re-input new password:");
+                
+                if (pw1 != pw2){
+                    alert("Passwords do not match");
+                    return;
+                }
+                
+                if(pw1.length<6){
+                    alert("Password is too short - 6 character minimum");
+                    return;
+                }
+                
+                document.getElementById("hiddenPw").value = pw1;
+                setPwButtonStyle();
+                alert('Password will be set when "Insert" is clicked');
+                return;
+            }
+        </script>
+<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" type="text/css" href="../css/styles.css"/>
@@ -43,14 +56,21 @@ $allList = $UM->getList();
             table, th, td{
                 border: 1px solid black;
             }
+
+            input[type = submit]{
+                margin: 0.0em;
+                font-size: smaller;
+                margin-right: 0em;
+            }
+
         </style>
-    </head>
-    <body>
+            </head>
+    <body onload="setPwButtonStyle();">
         <?php echo(file_get_contents('.\menu.html')) ?>
 
         <div><h2>Edit Users</h2></div>
         <!--display error message, if any-->
-        <?php if (isset($errMsg) && $errMsg != '') echo "<div><h3>$errMsg</h3><div>" ?>
+        <?php if (isset($errMsg) && $errMsg != '' && strtoupper($errMsg) != 'NONE') echo "<div><h3>$errMsg</h3><div>" ?>
         <div>
             <form action = "<?php filter_input(INPUT_SERVER, 'REQUEST_URI') ?>" method='post'>
                 <table>
@@ -68,28 +88,55 @@ $allList = $UM->getList();
                         <th>Zip</th>
                         <th>Phone</th>
                         <th>Email</th>
+                        <th>Password</th>
                     </tr>
                     <tr>
-                        
+
                         <td style="display:none"> <!-- hidden primary key for update/delete -->
                             <input type="hidden" name="userID" value='<?php echo($UM->userID) ?>'>
+                            <input type="hidden" name="pwHash" id="pwHash" value='<?php echo($UM->pwHash) ?>'>
+                            <input type="hidden" name="hiddenPw" id="hiddenPw" value=''>
                         </td>
-                        <td> <input type="text"  size="10" name="firstName" value='<?php echo($UM->firstName)?>' > </td>
-                        <td> <input type="text" size="10"  name="lastName"  value='<?php echo($UM->lastName) ?>' > </td>
-                        <td> <input type="text" size="10"  name="compOrg"   value='<?php echo($UM->compOrg)  ?>' > </td>
-                        <td> <input type="text" size="10"  name="address1"  value='<?php echo($UM->address1) ?>' > </td>
-                        <td> <input type="text" size="10"  name="address2"  value='<?php echo($UM->address2) ?>' > </td>
-                        <td> <input type="text" size="10"  name="city"      value='<?php echo($UM->city)     ?>' > </td>
-                        <td> <input type="text" size="1"   name="state"     value='<?php echo($UM->state)    ?>' > </td>
-                        <td> <input type="text" size="6"   name="zipCode"   value='<?php echo($UM->zipCode)  ?>' > </td>
-                        <td> <input type="text" size="10"  name="phone"     value='<?php echo($UM->phone)    ?>' > </td>
-                        <td> <input type="text" size="15"  name="email"     value='<?php echo($UM->email)    ?>' > </td>
+                        <td> 
+                            <input type="text" required style="width:100px;" name="firstName" value='<?php echo($UM->firstName) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="text" required style="width:100px;"  name="lastName"  value='<?php echo($UM->lastName) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="text" style="width:100px;"  name="compOrg"   value='<?php echo($UM->compOrg) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="text" required style="width:100px;"  name="address1"  value='<?php echo($UM->address1) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="text" style="width:100px;"  name="address2"  value='<?php echo($UM->address2) ?>' >
+                        </td>
+                        <td> 
+                            <input type="text" style="width:100px;"  name="city"      value='<?php echo($UM->city) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="text" style="width:40px;"   name="state"     value='<?php echo($UM->state) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="text" style="width:60px;"   name="zipCode" pattern="[0-9]{5}"  value='<?php echo($UM->zipCode) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="text" style="width:100px;"  name="phone" pattern="(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}" value='<?php echo($UM->phone) ?>' > 
+                        </td>
+                        <td> 
+                            <input type="email" required style="width:150px;"  name="email"     value='<?php echo($UM->email) ?>' > 
+                        </td>
+                        <!--<td>
+                            <input type='text' style="width:80px;" readonly name='pwFlag' id='pwFlag' 
+                                   value='<?php #echo($UM->pwHash==""?"Not Set":"Set") ?>'>
+                        </td>-->
                         <td>
-                            <input type="checkbox" name="cbAdmin"     <?php echo($UM->admin    =="1"?"checked":"") ?> >Admin</br>
-                            <input type="checkbox" name="cbAttend"    <?php echo($UM->attendee =="1"?"checked":"") ?> >Attendee</br>
-                            <input type="checkbox" name="cbPresenter" <?php echo($UM->presenter=="1"?"checked":"") ?> >Presenter</br>
-                            <input type="checkbox" name="cbStudent"   <?php echo($UM->student  =="1"?"checked":"") ?> >Student</br>
-                            <input type="checkbox" name="cbReviewer"  <?php echo($UM->reviewer =="1"?"checked":"") ?> >Reviewer</br>
+                            <input type="checkbox" name="cbAdmin"     <?php echo($UM->admin == "1" ? "checked" : "") ?> >Admin</br>
+                            <input type="checkbox" name="cbAttend"    <?php echo($UM->attendee == "1" ? "checked" : "") ?> >Attendee</br>
+                            <input type="checkbox" name="cbPresenter" <?php echo($UM->presenter == "1" ? "checked" : "") ?> >Presenter</br>
+                            <input type="checkbox" name="cbStudent"   <?php echo($UM->student == "1" ? "checked" : "") ?> >Student</br>
+                            <input type="checkbox" name="cbReviewer"  <?php echo($UM->reviewer == "1" ? "checked" : "") ?> >Reviewer</br>
                         </td>
                         <td> 
                             <?php
@@ -100,7 +147,10 @@ $allList = $UM->getList();
                                 echo('<input type="submit" name="btnDelete" value ="Delete"></br>');
                             }
                             ?>
-                            <input type="reset" name="btnClear" value ="Clear">
+                            <input type="reset" name="btnClear" value ="Reset"></br>                            
+                            <input type="button" name="btnPassword" id="btnPassword"
+                                   value='Set/Reset PW'
+                                   onclick="setPw();">
                         </td>
                     </tr>
                 </table>
@@ -118,11 +168,11 @@ $allList = $UM->getList();
                 <?php
                 foreach ($allList as $row) {
                     echo('<tr>');
-                    echo('<td>' . $row['ID'] . '</td>'); 
+                    echo('<td>' . $row['ID'] . '</td>');
                     echo('<td>' . "\n" .
-                        "<form action = " . $_SERVER['REQUEST_URI'] . " method='post'>" . "\n" .
-                        "<input type='hidden' name='userID' value=" . $row['ID'] . ">" . "\n" .
-                        "</form>" . "\n" .
+                    "<form action = " . $_SERVER['REQUEST_URI'] . " method='post'>" . "\n" .
+                    "<input type='hidden' name='userID' value=" . $row['ID'] . ">" . "\n" .
+                    "</form>" . "\n" .
                     "<form action = " . $_SERVER['REQUEST_URI'] . " method='post'>" . "\n" .
                     "<input type='hidden' name='userID' value=" . $row['ID'] . ">" . "\n" .
                     "<input type='text' name= 'firstName' value='" . $row['FIRST_NAME'] . "'></td>" . "\n" .
