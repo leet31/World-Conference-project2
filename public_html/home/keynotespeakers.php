e0264364e8297420021b460a37ab8635f1dae88a
Tiffany <Tiffany@10.17.158.86>
2016-11-13 17:20:27 -0500

    Update Online registration, font size, and style layout.

--- public_html/home/keynotespeakers.php
+++ public_html/home/keynotespeakers.php
@@ -56,17 +56,15 @@
             .dropdown:hover .dropdown-content {
                 display: block;
             }
-            .container{
-        overflow:hidden;
-        width:300px;/*change this to whatever you want*/
-        height:150px;/*change this to whatever you want*/
-
-        /*Of course, to get the desired effect, the size of the image "<img>" below must be larger than the size mentioned above*/
-    }
-
-    .container:hover{
-        overflow:visible
-    }
+           .thumbnail:hover {
+    position:relative;
+    top:25px;
+    left:25px;
+    width:300px;
+    height:auto;
+    display:block;
+    z-index:999;
+}
         </style>
     </head>
 
@@ -77,51 +75,35 @@
             <?php echo(file_get_contents('.\menu.html')) ?>
             
         </div>
-             <div class='container'><img src="../images/download.jpe"/></div>
+             <img src="../images/download.jpe" class="thumbnail" height="150" width="100"/>
                <div
                 <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;"><span style="color: rgba(255,255,255,1)">Grady Booch is an American software engineer best known for developing the UML (Unified Modeling Language) with Ivar Jacbson and James Rumbaugh. He is recognized internationally for his innovative work in software architecture, software engineering, and collaborative development. </span></p>
                 <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;">Topic of Discussion: <span style="font-style: normal; font-weight: bolder;">Software engineering</span>. &quot;Each Phase is the insurance of achievements towards success&quot;</p>
                </div>
-        </div>
-
-        <div id="note2">
-            <div class="dropdown2">
-                <img src="../images/Troy_Hunt.jpg" width="118" height="124" alt=""/>
-                <p style="text-align: right; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;"><span style="color: rgba(255,255,255,1)">Troy Hunt is an Australian web security expert. He is the creator of ASafaWeb, a tool that performs automated security analysis on ASP.NET websites. Hunt was named a Microsoft Most Valuable Professional (MVP) in Developer Security, and was recognized as a Microsoft MVP of the Year in 2011. He was also named a Microsoft Regional Director in 2016. </span></p>
+        
+                <img src="../images/Troy_Hunt.jpg" class="thumbnail" width="100" height="150"/>
+                <div
+                <p style="text-align: right; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;"><span style="color: rgba(255,255,255,1)">Troy Hunt is an Australian web security expert and the creator of ASafaWeb, a tool that performs automated security analysis on ASP.NET websites. Hunt was named a Microsoft Most Valuable Professional in Developer Security, and was recognized as a Microsoft MVP of the Year in 2011. He was also named a Microsoft Regional Director in 2016. </span></p>
                 <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;">Topic of Discussion:  <span style="font-style: normal; font-weight: bolder;">Computer Security</span>. &quot;Security, how far is to negligent&quot; </p>
-                <div class="dropdown-content2">
-                    <img src="../images/Troy_Hunt.jpg" alt="" width="250" height="200">
                 </div>
-            </div>
-        </div>
 
-        <div id="note3">
-            <div class="dropdown2">
-                <img src="../images/Wertheimer-Jeremy_250.jpg" width="131" height="146" alt=""/>
+                <img src="../images/Wertheimer-Jeremy_250.jpg" class="thumbnail" width="100" height="150" alt=""/>
+                  <div
                 <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;"><span style="color: rgba(255,255,255,1)">Jeremy Wertheimer founded and served as the CEO od ITA Software, a 400+ person software company based in Cambridge, MA, that powers the airfare searches on the websites of American Airlines, United Airlines, Southwest Airlines, US Airways, Orbitz, Kayak, Howire et al. Google acquired ITA Software in 2011. Jeremy is now the VP for Travel at Google. He is a trustee of Massachusetts Technology Leadership COuncil and a trustee of Maimonides school in Brookline, MA. He received a BE in Electrical Engineering from Cooper Union in 1982, an SM from MIT in COmputer Science, and PhD from MIT in Artificial Intelligence, with a minor in Neuroscience. </span></p>
                 <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;">Topic of Discussion: <span style="font-style: normal; font-weight: bolder;">Artificial Intelligence</span>. &quot;Where does humanity stand against artifical intelligence?&quot;</p>
-                <div class="dropdown-content2">
-                    <img src="../images/Wertheimer-Jeremy_250.jpg" alt="" width="250" height="200">
                 </div>
-
-                <div id="note4">
-                    <div class="dropdown2">
-                        <img src="../images/susan-kare-300.jpg" width="126" height="141" alt=""/>
+                
+                 <img src="../images/susan-kare-300.jpg" class="thumbnail" width="100" height="150" alt=""/>
+                  <div
                         <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;"><span style="color: rgba(255,255,255,1)">Susan Kare, a designer who helped bring the Apple computer to life with her sophiscated typography and iconic graphic skills. Working alongside Steve Jobs, she shaped many of the now-common interface elements of the Mac, like the command icon, which she found while looking through a book of symbols. </span></p>
                         <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;">Topic of Discussion: <span style="font-style: normal; font-weight: bolder;">Mobile Computing</span>. &quot;Mobile it shapes the way we evolve communications and intelligence.&quot;</p>
-                        <div class="dropdown-content2">
-                            <img src="../images/susan-kare-300.jpg" alt="" width="250" height="200">
-                        </div>
-
-                        <div id="note5">
-                            <div class="dropdown2">
-                                <img src="../images/stanislaw_raczynski_gawin.jpg" width="128" height="125" alt=""/>
-                                <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;"><span style="color: rgba(255,255,255,1)">Grady Booch is an Americ<strong>Stanislaw Raczynski</strong> is a professor of control theory, electronics and computer simulation at the Panamerican University in Mexico City. He is also the international director of the Socirty for Modelling Simulation in San Diego. He has authored 1 book, &ldquo;Simulacion por computadora&rdquo; and over 70 journal articles &amp; conference papers.an software engineer best known for developing the UML (Unified Modeling Language) with Ivar Jacbson and James Rumbaugh. He is recognized internationally for his innovative work in software architecture, software engineering, and collaborative development. </span></p>
-                                <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;">Topic of Discussion: <span style="font-style: normal; font-weight: normal;"><span style="font-weight: bold">Simulation and Modeling.</span> &quot;Where do we go from here?&quot;</span> </p>
-                                <div class="dropdown-content2">
-                                    <img src="../images/stanislaw_raczynski_gawin.jpg" width="250" height="200">
-                                </div>
-                            </div>
+                  </div>
+                 
+                  <img src="../images/stanislaw_raczynski_gawin.jpg" class="thumbnail" width="100" height="160" alt=""/>
+                   <div
+                         <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;"><span style="color: rgba(255,255,255,1)">Grady Booch is an Americ<strong>Stanislaw Raczynski</strong> is a professor of control theory, electronics and computer simulation at the Panamerican University in Mexico City. He is also the international director of the Socirty for Modelling Simulation in San Diego. He has authored 1 book, &ldquo;Simulacion por computadora&rdquo; and over 70 journal articles &amp; conference papers.an software engineer best known for developing the UML (Unified Modeling Language) with Ivar Jacbson and James Rumbaugh. He is recognized internationally for his innovative work in software architecture, software engineering, and collaborative development. </span></p>
+                         <p style="text-align: left; font-family: Baskerville, 'Palatino Linotype', Palatino, 'Century Schoolbook L', 'Times New Roman', serif; font-style: normal; font-size: 14px;">Topic of Discussion: <span style="font-style: normal; font-weight: normal;"><span style="font-weight: bold">Simulation and Modeling.</span> &quot;Where do we go from here?&quot;</span> </p>
+                   </div>
                             </table>
                             </body>
                             </html>
