cdd7793a634bd1e58ad7595dcdf0999ee00a67fc
Tiffany <Tiffany@10.17.158.86>
2016-11-14 18:05:02 -0500

    Merge origin/master
    Conflicts:
    	public_html/home/keynotespeakers.php

--- public_html/home/conferenceprogram.php
+++ public_html/home/conferenceprogram.php
@@ -12,7 +12,12 @@
         <p style="text-align: center; font-size: 36px;">Conference Program</p>
         <?php include '../home/menu.php'; ?>
         <div>
-            TBD
+        <p style="font-size: x-large">&nbsp;</p>
+        <p style="font-size: x-large"><strong>Program</strong></p>
+        <p style="font-size: x-large">&nbsp;</p>
+        <p><strong> Each conference is divided into a number of sessions. These sessions are not necessarily scheduled in the same room/location. Therefore, conference attendees are to check the location of the session(s) they wish to attend - The conference room numbers are included for each session. There are a number of sessions which may be of significant interest to every participants. We also encourage you to check the schedules for other joint conferences. </strong></p>
+        <p>&nbsp;</p>
+        <p><strong>To download the entire program as PDF: </strong></p>
         </div>
         
     </body>
