2eb1a29af2235b094cfc52c72c6ca4e3a6c6e371
Tiffany <Tiffany@10.17.158.86>
2016-11-15 17:28:55 -0500

    Merge origin/master
    Conflicts:
    	public_html/home/keynotespeakers.php

--- public_html/home/index.php
+++ public_html/home/index.php
@@ -7,13 +7,19 @@
     </head>
     <body background="../images/2015_AIGA-Design-Month_Website-Footer.png">
         <p style="text-align: center; font-size: xx-large; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif;">
-            <strong><em>World Congress CS-IT Conferences</em></strong>
+            <strong><em>WORLD CONGRESS COMPUTER CONFERENCE 2016</em></strong>    
         </p>
+        <p style="text-align: center; font-size: large; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif;">
+        <b>November 20-28, Charlotte, NC</b></p>
        
         <?php include '../home/menu.php'?>
+       
+        <p style="font-size: x-large">&nbsp;</p>
+        <p style="font-size: xx-large; background-color:#D49318; color: cyan;"><strong>The 2016 World Congress in Computer science, Computer Engineering, & Applied Computing</strong></p>
+        <p style="font-size: x-large">&nbsp;</p>
+        <p><strong>  </strong></p>
         <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <?php echo"<p><h1>Hello PHP from Kevin!</h1>"; ?>
+     
     </body>
 
 </html>
