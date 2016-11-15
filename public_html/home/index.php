948136e115262ce6d6b193742ccfce0ae4917f9a
Tiffany <Tiffany@10.17.158.86>
2016-11-15 18:44:36 -0500

    Merge origin/master
    Conflicts:
    	public_html/home/keynotespeakers.php

--- public_html/home/index.php
+++ public_html/home/index.php
@@ -13,12 +13,11 @@
         <b>November 20-28, Charlotte, NC</b></p>
        
         <?php include '../home/menu.php'?>
-       <div
+      
         <p style="font-size: x-large">&nbsp;</p>
-        <p style="font-size: xx-large; background-color:#4880D5; color: black;"><strong>The 2016 World Congress in Computer science, Computer Engineering, & Applied Computing</strong></p>
-        <p style="font-size: x-large">&nbsp;</p>
-         <p style="font-size: large; color: black;"><i>Welcome to the 2016 World Congress in Computer Science. This years events will be composed of research presentations, keynote lectures, tutorials, and panel discussions. There will be some <a href="../home/keynotespeakers.php">Guest Speakers</a> that will be attending the conference, in conjunction with many more. Also, take advantage of early registeration and early bookings at the hotel where this years conference is being held. We will provide the information (map of rooms, list of workshops, room numbers, additional workshops of interest,) included in a catalog package before the day of conference.</i></p>
-       </div>   
+        <p style="text-align: center; font-size: xx-large; border-bottom:6px solid red; background-color:lightgray; color: black;"><strong>The 2016 World Congress in Computer science, Computer Engineering, & Applied Computing</strong></p>
+         <p style="font-size: large; border-bottom:6px solid red; background-color:lightgray; color: black;"><i>Welcome to the 2016 World Congress in Computer Science. This years events will be composed of research presentations, keynote lectures, tutorials, and panel discussions. There will be some <a href="../home/keynotespeakers.php">Guest Speakers</a> that will be attending the conference, in conjunction with many more. Also, take advantage of early registeration and early bookings at the hotel where this years conference is being held. We will provide the information (map of rooms, list of workshops, room numbers, additional workshops of interest,) included in a catalog package before the day of conference.</i></p>
+        
                    
 
              
