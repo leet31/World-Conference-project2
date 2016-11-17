d0b13774b96b5aac6021d7092bec7a6ee5d6bebc
Cici R <cuicuiruan@gmail.com>
2016-11-17 00:13:30 -0500

    view fix

--- public_html/store/index.php
+++ public_html/store/index.php
@@ -71,7 +71,7 @@ switch ($action) {
     case 'overview':
         $category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
         if ($category_id == NULL || $category_id == FALSE) {
-            $category_id = 1;
+            $category_id = 15;
         }
         $category_one = $CM->getListByID($category_id);
         $product_list = $PM->getListByCat($category_id);
